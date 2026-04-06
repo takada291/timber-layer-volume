b5f36ebdac323c8737fe50a8900ad894eb76f5e6
takada291 <tkp291@gmail.com>
2026-04-06 13:53:03 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'timber-layer-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // キャッシュにあればそれを返し、なければネットワークから取得
+        return response || fetch(event.request);
+      })
+  );
+});
